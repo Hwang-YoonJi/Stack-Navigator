e62ddb05f6c3473273b73a8e82c2b69c0e3da793
Hwang-yoonji <kaci00@naver.com>
2021-07-20 14:42:04 +0900

    stack

--- App.js
+++ App.js
@@ -1,21 +1,75 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+function HomeScreen({navigation}) {
+  const onPressHandler = () => {
+    navigation.navigate('Setting');
+  }
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.body}>
+      <Text style={styles.text}>Home Screen</Text>
+      <Pressable onPress={onPressHandler} style={({pressed})=>({backgroundColor: pressed? '#ede7f6':'#b388ff'})}>
+        <Text style={styles.button}>Go to Setting</Text>
+      </Pressable>
     </View>
   );
 }
 
+function SettingScreen({navigation}) {
+  const onPressHandler = () => {
+    navigation.goBack();
+  }
+  return (
+    <View style={styles.body}>
+      <Text style={styles.text}>Setting Screen</Text>
+      <Pressable onPress={onPressHandler} style={({pressed})=>({backgroundColor: pressed? '#ede7f6':'#b388ff'})}>
+        <Text style={styles.button}>Go Back to Home</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={HomeScreen} options={{ header: () => null }} />
+        <Stack.Screen name="Setting" component={SettingScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    alignItems: 'center',
   },
+  text: {
+    fontSize: 40,
+    fontWeight: 'bold',
+    margin: 50,
+  },
+  button: {
+    fontSize: 20,
+    margin: 10,
+  }
 });
+
+export default App;
+
+/*
+Pressable은 기본적으로 touchableopacity와 같이 버튼의 역할을 하지만, 더 다양한 press 단계를 추적할 수 있다.
+
+onPressIn: press가 활성화되었을 때
+onPressOut: press 제스처가 비활성화되었을 때
+
+onPressIn 된 이후에는 다음과 같은 스텝이 진행된다:
+1) 유저가 손가락을 때면, onPressOut가 바로 따라옴
+2) 유저가 손가락을 500 milliseconds 이상 누르고 있다가 때면, onLongPress가 바로 작동된다.
+*/
---
+++ README.md
@@ -0,0 +1,12 @@
+# React Native - Stack Navigator
+Stack Navigator Study :)
+
+## Installation
+
+```bash
+npm install @react-navigation/native
+```
+
+```bash
+npm install @react-navigation/stack
+```
